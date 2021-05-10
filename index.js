40f7eee070bc2c553fd9a5ca92f90d48ce437143
Victor <lausellv@yahoo.com>
2021-05-10 22:40:34 +0200

    added error and return

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const superagent = require("superagent");
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-    console.log(res.body.message);
-  });
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        console.log('Random dog name saved');
+      });
+    });
 });
-
-// // console.log(res.body.message);
-
-// //       fs.writeFile('dog-img.txt', res.body.message, (err) => {
-// //         if (err) return  console.log(err.message);
-// //         console.log('Random dog name saved');
-// //       });
 // //   }).catch(err => {
 // //     console.log(err.message);
 // //   });
