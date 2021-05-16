37cb498ae394a00bb2d6e0ad59472ee669a6c042
Victor <lausellv@yahoo.com>
2021-05-16 17:03:23 +0200

    superagent review

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const superagent = require("superagent");
+const fs = require('fs');
+const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
@@ -9,6 +9,7 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
       if (err) return console.log(err.message);
       console.log(res.body.message);
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
         console.log('Random dog name saved');
       });
     });
